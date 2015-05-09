4d048da8cdb5b72b05223cdd95dbfeeb512e0dfe
anayelii <a.flores1591@gmail.com>
2015-05-09 01:27:18 -0400

    Refactored function to get the Titanic file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 /* lets do stuff */
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
 #[allow(dead_code)]
 
@@ -10,25 +14,27 @@ struct Survivor {
   frequency: u16,
 }
 
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-
-fn main() {
-  let path = Path::new("lib/Titanic.csv");
+fn get_file(file_path: &'static str) -> File {
+  let path = Path::new(file_path);
   let display = path.display();
 
-  let mut file = match File::open(&path) {
+  match File::open(&path) {
     Err(why) => panic!("Couldn't open {}: {}", display, Error::description(&why)),
     Ok(file) => file,
-  };
+  }
+}
 
+fn stringify(mut file: File) -> String {
   let mut s = String::new();
   match file.read_to_string(&mut s) {
-    Err(why) => panic!("couldn't read {}: {} ", display, Error::description(&why)),
-    Ok(_) => print!(" {} contains:\n{}", display, s),
+    Err(why) => panic!("couldn't read {} ", Error::description(&why)),
+    Ok(_) => s
   }
+}
 
 
+fn main() {
+  let f = get_file("lib/Titanic.csv");
+  let s = stringify(f);
+  println!("Titanic {}", s);
 }
